e3532f49ea093b618e01720519290f68777aad14
Gleidson Viana da Cruz <134108223+GleidsonVian@users.noreply.github.com>
2025-03-11 18:51:23 -0300

    Add files via upload

---
+++ MedidasRetangulo/Program.cs
@@ -0,0 +1,23 @@
+﻿using RetanguloExercicio1;
+using System.Globalization;
+
+namespace Exercicio
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Retangulo ret = new Retangulo();
+            Console.WriteLine("Entre a largura do retângulo:");
+            ret.Largura = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entre a altura do retângulo:");
+            ret.Altura = double.Parse(Console.ReadLine());
+            Console.WriteLine("AREA = " + ret.Area().ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine("PERÍMETRo = " + ret.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + ret.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+        }
+    }
+}
---
+++ MedidasRetangulo/Retangulo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace RetanguloExercicio1
+{
+    public class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+
+        public double Perimetro()
+        {
+            return 2 * (Largura + Altura);
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Math.Pow(Altura, 2) + Math.Pow(Largura, 2));
+        }
+
+    }
+}
