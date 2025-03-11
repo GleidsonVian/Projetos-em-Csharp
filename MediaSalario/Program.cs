5903d10f284c816672166ea0f585ca0d04184bda
Gleidson Viana da Cruz <134108223+GleidsonVian@users.noreply.github.com>
2025-03-11 18:51:52 -0300

    Add files via upload

---
+++ MediaSalario/Program.cs
@@ -0,0 +1,29 @@
+﻿using exercicio2._0;
+
+namespace exercicio2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MediaSalario p1 = new MediaSalario();
+            MediaSalario p2 = new MediaSalario();
+
+            Console.WriteLine("Dados do primeiro funcionário:");
+            Console.Write("Nome: ");
+            p1.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            p1.Salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do segundo funcionário:");
+            Console.Write("Nome: ");
+            p2.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            p2.Salario = double.Parse(Console.ReadLine());
+
+            double salarioMedio = (p1.Salario + p2.Salario) / 2;
+
+            Console.WriteLine($"Salário médio = {salarioMedio}");
+        }
+    }
+}
