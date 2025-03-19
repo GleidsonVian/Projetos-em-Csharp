84bebb917018bb3c3991e54c76a4e6faeb8a8046
Gleidson Viana da Cruz <134108223+GleidsonVian@users.noreply.github.com>
2025-03-19 18:29:23 -0300

    Add files via upload

---
+++ Armazenando e Convertendo Dados Em Bytes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Diagnostics;
+using System.Text;
+using System;
+
+namespace Curso
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Peça ao usuário uma frase.
+            Console.WriteLine("Digite uma frase");
+            string frase = Console.ReadLine();
+            //Converta essa frase em bytes usando Encoding.UTF8.GetBytes() e salve no arquivo "bytes.txt" com File.WriteAllBytes().
+            byte[] bytes = Encoding.UTF8.GetBytes(frase);
+            File.WriteAllBytes("bytes.txt",bytes);
+            //Leia o arquivo, converta os bytes novamente para string e exiba no console
+            byte[] readBytes = File.ReadAllBytes("bytes.txt");
+            string byteConvertidoParaString = Encoding.UTF8.GetString(readBytes);
+            Console.WriteLine(byteConvertidoParaString);
+        }
+    }
+}
