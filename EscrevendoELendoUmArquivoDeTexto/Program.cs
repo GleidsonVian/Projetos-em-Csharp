55187f944276b15c49a03dbc728cf5fa3f58bd5d
Gleidson Viana da Cruz <134108223+GleidsonVian@users.noreply.github.com>
2025-03-19 18:27:41 -0300

    Add files via upload

---
+++ EscrevendoELendoUmArquivoDeTexto/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.IO;
+using System.Text;
+
+namespace curso
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            {
+                // Solicite ao usuário que digite um texto.
+                Console.WriteLine("Digite um texto para escrever num arquivo txt");
+               string texto = Console.ReadLine();
+                // Salve esse texto em um arquivo chamado "meuarquivo.txt" usando File.WriteAllText().
+                File.WriteAllText("meuarquivo.txt", texto);
+                // Leia o conteúdo do arquivo e exiba no console.
+                string conteudo = File.ReadAllText("meuarquivo.txt");
+                Console.WriteLine(conteudo);
+            }
+        }
+    }
+}
