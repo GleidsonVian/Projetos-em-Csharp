8fb38fe185a62ee9fa71f089148256b77c435309
Gleidson Viana da Cruz <134108223+GleidsonVian@users.noreply.github.com>
2025-03-19 18:30:37 -0300

    Add files via upload

---
+++ Contador de Linhas em um Arquivo/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Leia um arquivo chamado "dados.txt"(crie o arquivo antes com algumas linhas de texto).
+            string[] dados = File.ReadAllLines("dados.txt");
+            //Conte quantas linhas há no arquivo e exiba no console a quantidade.
+            int linhas = 0;
+            foreach (var item in dados)
+            {
+                linhas += 1;
+            }
+            Console.WriteLine("Total de linhas no arquivo: " + linhas);
+        }
+    }
+}
