7493d4a4f790bc8d181952c0b41711bd09e2569c
Gleidson Viana da Cruz <134108223+GleidsonVian@users.noreply.github.com>
2025-03-19 18:30:05 -0300

    Add files via upload

---
+++ Criando um Log de Acessos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using Microsoft.Win32;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Curso
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Sempre que executado, registre a data e hora da execução no arquivo "log.txt", adicionando uma nova linha a cada execução.
+            DateTime horaAtual = DateTime.Now;
+            
+            File.AppendAllText("log.txt", horaAtual.ToString());
+            //Após registrar o acesso, leia e exiba todo o histórico de acessos.
+            string[] logLido = File.ReadAllLines("log.txt");
+            foreach (var item in logLido)
+            {
+                Console.WriteLine(item );
+            }
+        }
+    }
+}
