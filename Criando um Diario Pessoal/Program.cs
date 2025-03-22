86170af5bdd62211b8f1c4b433f08f4254f1fa17
Gleidson Viana da Cruz <134108223+GleidsonVian@users.noreply.github.com>
2025-03-22 18:02:01 -0300

    Add files via upload

---
+++ Criando um Diario Pessoal/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+
+namespace curso
+{
+    public class Program
+    {
+        public static void Main() {
+
+            
+
+            using(FileStream fluxo = new FileStream("diario.txt", FileMode.Create, FileAccess.Write))
+                using(StreamWriter escritor = new StreamWriter(fluxo))
+                
+            {
+                while (true)
+                {
+                    Console.WriteLine("Digite (f) para sair do programa ou (c) para continuar");
+                    string continuar = Console.ReadLine();
+
+                    if (continuar.ToLower() == "f") {
+                        break;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Criando um Diário Pessoal");
+                        Console.WriteLine("Escreva uma entrada de diário");
+                        DateTime horaAtual = DateTime.Now;
+                        escritor.WriteLine(horaAtual.ToString());
+                        string entrada = Console.ReadLine();
+                        escritor.WriteLine(entrada);
+                        Console.Clear();
+                    }
+                }
+
+
+                
+            }
+            Console.Clear();
+            StreamReader leitor = new StreamReader("diario.txt");
+            Console.WriteLine(leitor.ReadToEnd());
+
+        }
+    }
+}
