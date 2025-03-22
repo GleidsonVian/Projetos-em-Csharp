f1b97106c42410ef912601db2534f27049984cca
Gleidson Viana da Cruz <134108223+GleidsonVian@users.noreply.github.com>
2025-03-21 21:32:12 -0300

    Add files via upload

---
+++ Contador de Palavras em um Arquivo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace curso
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using(StreamReader leitor =  new StreamReader("texto.txt")) 
+            {
+                string linha;
+                int quantidadeLinhas =0 ;
+
+                while ((linha = leitor.ReadLine()) != null)
+                {
+                    quantidadeLinhas += 1;
+
+                }
+                Console.WriteLine("Quantidade de linhas: " + quantidadeLinhas);
+            }
+        }
+    }
+    } 
