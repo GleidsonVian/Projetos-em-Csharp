92d38d00c69a5143a424b0c437cf0373ca21bc96
Gleidson Viana da Cruz <134108223+GleidsonVian@users.noreply.github.com>
2025-03-24 00:45:25 -0300

    Add files via upload

---
+++ Copiar Arquivo Linha por Linha/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace curso
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using(StreamReader leitor = new StreamReader("original.txt"))
+            using (StreamWriter escritor = new StreamWriter("copia.txt"))
+            {
+                escritor.Write(leitor.ReadToEnd());
+
+                    
+                }
+            StreamReader leitorCopia = new StreamReader("copia.txt");
+            string linha;
+            int quantidadeLinha = 0;
+            while ((linha = leitorCopia.ReadLine()) != null)
+            {
+                quantidadeLinha += 1;
+            }
+            Console.WriteLine("Foram copiadas um total de " + quantidadeLinha + " linhas");
+        }
+        }
+    }
